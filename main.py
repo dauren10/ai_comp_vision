45f4a2e46c9477e7a63974f2b38ff05f4bee2434
Dauren Shalabayev <dauren.shalabayev@gmail.com>
2023-08-05 14:26:43 +0600

    Init

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+
+# img =  cv2.imread('images/1.jpg')
+# cv2.imshow('Result',img)
+
+# cv2.waitKey(0)
+'''for video
+cap  = cv2.VideoCapture('videos/rally.mp4')
+'''
+'''for camera'''
+cap  = cv2.VideoCapture(0)
+cap.set(3, 500)
+cap.set(4, 300)
+while True:
+    success, img = cap.read()
+    cv2.imshow('Result', img)
+
+    if cv2.waitKey(1) &  0xFF == ord('q'):
+        break
